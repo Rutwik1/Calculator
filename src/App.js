367ed2c609a26ed0c1f283a1458904c51bb27af8
Rutwik <rutkalbandhe@gmail.com>
2023-04-03 09:16:52 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const Calculator = () => {
+  const [result, setResult] = useState("0");
+  const [operator, setOperator] = useState(null);
+  const [previousValue, setPreviousValue] = useState(null);
+
+  const handleDigitClick = (digit) => {
+    if (result === "0") {
+      setResult(digit.toString());
+    } else {
+      setResult(result.toString() + digit.toString());
+    }
+  };
+
+  const handleOperatorClick = (op) => {
+    setOperator(op);
+    setPreviousValue(result);
+    setResult("0");
+  };
+
+  const handleClearClick = () => {
+    setResult("0");
+    setOperator(null);
+    setPreviousValue(null);
+  };
+
+  const handleEqualClick = () => {
+    if (operator === "+") {
+      setResult((parseFloat(previousValue) + parseFloat(result)).toString());
+    } else if (operator === "-") {
+      setResult((parseFloat(previousValue) - parseFloat(result)).toString());
+    } else if (operator === "*") {
+      setResult((parseFloat(previousValue) * parseFloat(result)).toString());
+    } else if (operator === "/") {
+      setResult((parseFloat(previousValue) / parseFloat(result)).toString());
+    }
+    setOperator(null);
+    setPreviousValue(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <div className="display">{result}</div>
+      <div className="keypad">
+        <button className="operator" onClick={() => handleOperatorClick("+")}>
+          +
+        </button>
+        <button className="operator" onClick={() => handleOperatorClick("-")}>
+          -
+        </button>
+        <button className="operator" onClick={() => handleOperatorClick("*")}>
+          *
+        </button>
+        <button className="operator" onClick={() => handleOperatorClick("/")}>
+          /
+        </button>
+        <button onClick={() => handleDigitClick(7)}>7</button>
+        <button onClick={() => handleDigitClick(8)}>8</button>
+        <button onClick={() => handleDigitClick(9)}>9</button>
+        <button onClick={() => handleDigitClick(4)}>4</button>
+        <button onClick={() => handleDigitClick(5)}>5</button>
+        <button onClick={() => handleDigitClick(6)}>6</button>
+        <button onClick={() => handleDigitClick(1)}>1</button>
+        <button onClick={() => handleDigitClick(2)}>2</button>
+        <button onClick={() => handleDigitClick(3)}>3</button>
+        <button onClick={() => handleDigitClick(0)}>0</button>
+        <button className="clear" onClick={handleClearClick}>
+          C
+        </button>
+        <button className="equal" onClick={handleEqualClick}>
+          =
+        </button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Calculator;
